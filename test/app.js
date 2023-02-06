3b0e0969f27a7119ae91259e901200c82fb74c85
echoedinvoker <echoedinvoker@gmail.com>
2023-02-06 13:10:14 +0800

    qq

--- test/app.js
+++ test/app.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.post('/test', (req, res) => {
+app.post("/test", (req, res) => {
   console.log(req.body);
-  res.status(200).json({ message: 'Test POST method success!' });
+  res.status(200).json({
+    title: "node_test",
+    status: "success",
+    data: [
+      {
+        user: "u01",
+        msg: "msg1",
+      },
+      {
+        user: "u02",
+        msg: "msg2",
+      },
+    ],
+  });
 });
 
 app.listen(3600, () => {
-  console.log('Server is running on port 3600');
+  console.log("Server is running on port 3600");
 });
-
